fb9c4aa9fa680225f486d17c76f75949d4e12b36
Sravan <sravanjangapally40@gmail.com>
2024-12-16 19:50:42 +0530

    simple java program

---
+++ Simplejava.js
@@ -0,0 +1,14 @@
+public class Simplejava {
+    public static void main(String[] args) {
+        // Declare and initialize two numbers
+        int number1 = 10; // You can change this value
+        int number2 = 20; // You can change this value
+
+        // Calculate the sum
+        int sum = number1 + number2;
+
+        // Display the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is " + sum);
+    }
+}
+
